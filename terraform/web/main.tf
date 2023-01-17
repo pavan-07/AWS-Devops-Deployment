fb09e4a54db88b55fc27da274a385e0dec0ddead
pavan-07 <bpavan.fab4@gmail.com>
2023-01-16 22:47:00 -0800

    new changes

---
+++ terraform/web/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "devops01_web" {
+    ami = "ami-06878d265978313ca"
+    instance_type = "t2.micro"
+    key_name = "devops01"
+
+    tags = {
+        Name = "${var.name}"
+    }
+}
